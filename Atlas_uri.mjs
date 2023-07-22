b4e2d6ffa47b42c13be896d52070d806b3adcab2
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-22 22:11:04 +0500

    edit

--- Atlas_uri.mjs
+++
@@ -1,5 +0,0 @@
-const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.q7hzrsw.mongodb.net/?retryWrites=true&w=majority`
-const client = new MongoClient(mongodbURI);
-const database = client.db('ecom');
-const productsCollection = database.collection('products');
-export default mongodbUri;
--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-import type { ObjectId } from "mongodb";
-
-export interface IProduct {
-  _id?: ObjectId;
-  name: string;
-  price: number;
-  description: string;
-}
