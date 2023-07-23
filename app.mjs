b82aa0526352002d7bc5de4c3aea8a19b4d803b4
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-23 17:34:19 +0500

    edit

--- app.mjs
+++ app.mjs
@@ -1,40 +1,71 @@
-import express from 'express';
-import { nanoid } from 'nanoid';
-import cors from 'cors';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
+// app.mjs
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// Import required libraries
+import Notyf from 'notyf';
+import axios from 'axios';
 
-// Sample initial data
-let products = [
-  //...
-];
+// Function to display students data
+function displayStudents() {
+  // Implement code to fetch data from the server (e.g., API) using axios
+  // Update the studentsData array with the fetched data
 
-// Serve the index.html page for the root URL
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+  // Code to generate HTML elements for each student record and append it to the dataContainer
+}
 
-app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, 'index.html'));
+// Function to add a new student
+function addStudent(event) {
+  // Implement code to handle form submission and add a new student record to the server (e.g., API) using axios
+  // Refresh the displayed list of students after a successful addition
+
+  // Show a success notification using Notyf
+}
+
+// Function to edit a student
+function editStudent(studentId) {
+  // Implement code to handle the edit functionality for a specific student record
+  // Update the student data on the server (e.g., API) using axios
+  // Refresh the displayed list of students after a successful edit
+
+  // Show a success notification using Notyf
+}
+
+// Function to delete a student
+function deleteStudent(studentId) {
+  // Implement code to handle the delete functionality for a specific student record
+  // Remove the student data from the server (e.g., API) using axios
+  // Refresh the displayed list of students after a successful deletion
+
+  // Show a success notification using Notyf
+}
+
+// Function to search for students
+function searchStudents(searchQuery) {
+  // Implement code to filter the studentsData array based on the searchQuery
+  // Display the filtered results in the dataContainer
+}
+
+// Initialize Notyf for notifications
+const notyf = new Notyf();
+
+// Sample data (You can replace this with data fetched from the server)
+let studentsData = [];
+
+// Get references to HTML elements
+const addStudentForm = document.getElementById('addStudentForm');
+const dataContainer = document.getElementById('data');
+const searchInput = document.getElementById('searchInput');
+
+// Event listener for form submission to add a new student
+addStudentForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  addStudent();
 });
 
-// Get all products
-app.get('/api/v1/products', (req, res) => {
-  res.json({ data: products });
+// Event listener for search input
+searchInput.addEventListener('input', (event) => {
+  const searchQuery = event.target.value.trim().toLowerCase();
+  searchStudents(searchQuery);
 });
 
-// Create a new product
-app.post('/api/v1/product', (req, res) => {
-  //...
-});
-
-// Delete a product
-app.delete('/api/v1/product/:id', (req, res) => {
-  //...
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Call the display function initially to show existing data
+displayStudents();
