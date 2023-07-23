1297dfd18a1d7dcb0f22b16f383528d90e3f4780
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-24 03:49:40 +0500

    fetching data on mongodb

--- public/script.mjs
+++ public/script.mjs
@@ -47,3 +47,52 @@ document.getElementById("teleForm").addEventListener("submit", async (event) => 
     alert("Failed to add product. Please try again.");
   }
 });
+
+// script.mjs
+// ... (previous code)
+
+// Function to fetch all products from the server
+async function fetchProducts() {
+  try {
+    const response = await fetch("/products");
+    const data = await response.json();
+
+    if (response.ok) {
+      // Display the products in the table
+      const productList = data.data;
+      const productTable = document.getElementById("product-list");
+      productTable.innerHTML = "";
+
+      productList.forEach((product) => {
+        const row = productTable.insertRow();
+        row.innerHTML = `
+          <td>${product.id}</td>
+          <td>${product.name}</td>
+          <td>${product.brand}</td>
+          <td>${product.model}</td>
+          <td>${product.price}</td>
+          <td>
+            <button onclick="editProduct('${product.id}')">Edit</button>
+            <button onclick="deleteProduct('${product.id}')">Delete</button>
+          </td>
+        `;
+      });
+    } else {
+      // Handle error if fetching products fails
+      console.log("Error fetching products:", data.message);
+    }
+  } catch (error) {
+    console.error("Error fetching products:", error);
+  }
+}
+
+// Function to refresh the products table after adding, editing, or deleting a product
+function refreshProductTable() {
+  fetchProducts();
+}
+
+// ... (other functions and event listeners)
+
+// Call the fetchProducts function when the page loads to display the products
+fetchProducts();
+
