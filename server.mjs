0717eeb7a4c3f7b4afbd905318f087870499532a
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-24 01:07:26 +0500

    edit

--- server.mjs
+++ server.mjs
@@ -1,42 +1,190 @@
-const products = [];
+import express from "express";
+import { customAlphabet } from 'nanoid'
+const nanoid = customAlphabet('1234567890', 20)
+import { MongoClient } from "mongodb"
 
-export const getAllProducts = (_req, res) => {
-  res.json(products);
-};
+import './config/index.mjs'
 
-export const addProduct = (req, res) => {
-  const { name, price, description } = req.body;
-  const newProduct = {
-    id: Date.now().toString(),
-    name,
-    price,
-    description,
-  };
-  products.push(newProduct);
-  res.status(201).json(newProduct);
-};
+const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.bykenlf.mongodb.net/?retryWrites=true&w=majority`
+const client = new MongoClient(mongodbURI);
+const database = client.db('ecom');
+const productsCollection = database.collection('products');
 
-export const updateProduct = (req, res) => {
-  const { id } = req.params;
-  const { name, price, description } = req.body;
-  const product = products.find((product) => product.id === id);
-  if (!product) {
-    res.status(404).json({ message: 'Product not found' });
-    return;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+
+
+let products = [
+  {
+    id: nanoid(), // always a number
+    name: "abc product",
+    price: "$23.12",
+    description: "abc product description"
   }
-  product.name = name;
-  product.price = price;
-  product.description = description;
-  res.json(product);
-};
+];
 
-export const deleteProduct = (req, res) => {
-  const { id } = req.params;
-  const index = products.findIndex((product) => product.id === id);
-  if (index === -1) {
-    res.status(404).json({ message: 'Product not found' });
-    return;
+app.get("/products", (req, res) => {
+  res.send({
+    message: "all products",
+    data: products
+  });
+});
+
+//  https://baseurl.com/product/1231
+app.get("/product/:id", (req, res) => {
+  console.log(typeof req.params.id)
+
+  if (isNaN(req.params.id)) {
+    res.status(403).send("invalid product id")
   }
-  products.splice(index, 1);
-  res.json({ message: 'Product deleted successfully' });
-};
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+    res.send({
+      message: "product found with id: " + products[isFound].id,
+      data: products[isFound]
+    });
+  }
+});
+
+app.post("/product", async (req, res) => {
+
+  // {
+  //   id: 212342, // always a number
+  //   name: "abc product",
+  //   price: "$23.12",
+  //   description: "abc product description"
+  // }
+
+
+  if (!req.body.name
+    || !req.body.price
+    || !req.body.description) {
+
+    res.status(403).send(`
+      required parameter missing. example JSON request body:
+      {
+        name: "abc product",
+        price: "$23.12",
+        description: "abc product description"
+      }`);
+  }
+
+  const doc = {
+    id: nanoid(),
+    name: req.body.name,
+    price: req.body.price,
+    description: req.body.description,
+  }
+  const result = await productsCollection.insertOne(doc);
+
+
+
+  // products.push({
+  //   id: nanoid(),
+  //   name: req.body.name,
+  //   price: req.body.price,
+  //   description: req.body.description,
+  // });
+
+
+  res.status(201).send({ message: "created product" });
+});
+
+app.put("/product/:id", (req, res) => {
+
+  if (
+    !req.body.name
+    && !req.body.price
+    && !req.body.description) {
+
+    res.status(403).send(`
+      required parameter missing. 
+      atleast one parameter is required: name, price or description to complete update
+      example JSON request body:
+      {
+        name: "abc product",
+        price: "$23.12",
+        description: "abc product description"
+      }`);
+  }
+
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+
+    if (req.body.name) products[isFound].name = req.body.name
+    if (req.body.price) products[isFound].price = req.body.price
+    if (req.body.description) products[isFound].description = req.body.description
+
+    res.send({
+      message: "product is updated with id: " + products[isFound].id,
+      data: products[isFound]
+    });
+  }
+});
+
+app.delete("/product/:id", (req, res) => {
+
+  let isFound = false;
+
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === req.params.id) {
+      isFound = i;
+      break;
+    }
+  }
+
+  if (isFound === false) {
+    res.status(404);
+    res.send({
+      message: "product not found"
+    });
+  } else {
+    products.splice(isFound, 1)
+
+    res.send({
+      message: "product is deleted"
+    });
+  }
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
