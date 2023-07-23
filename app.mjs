593a68b3795bf58c1214c5c57461fa8af294338a
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-23 18:00:08 +0500

    add functions in js

--- app.mjs
+++ app.mjs
@@ -5,43 +5,136 @@ import Notyf from 'notyf';
 import axios from 'axios';
 
 // Function to display students data
-function displayStudents() {
-  // Implement code to fetch data from the server (e.g., API) using axios
-  // Update the studentsData array with the fetched data
+async function displayStudents() {
+  try {
+    const response = await axios.get('/api/students'); // Replace '/api/students' with your API endpoint to fetch student data
+    studentsData = response.data; // Assuming the response contains an array of student records
+    renderStudents();
+  } catch (error) {
+    console.error('Error fetching students data:', error);
+  }
+}
 
-  // Code to generate HTML elements for each student record and append it to the dataContainer
+// Function to render students data
+function renderStudents() {
+  dataContainer.innerHTML = ''; // Clear the previous data
+  studentsData.forEach((student) => {
+    const studentElement = document.createElement('div');
+    studentElement.innerHTML = `
+      <p>Student Name: ${student.name}</p>
+      <p>Course Fee: ${student.courseFee}</p>
+      <p>Course Title: ${student.courseTitle}</p>
+      <p>Batch Number: ${student.batchNumber}</p>
+      <button class="edit-button" data-id="${student.id}">Edit</button>
+      <button class="delete-button" data-id="${student.id}">Delete</button>
+      <hr>
+    `;
+    dataContainer.appendChild(studentElement);
+  });
 }
 
 // Function to add a new student
-function addStudent(event) {
-  // Implement code to handle form submission and add a new student record to the server (e.g., API) using axios
-  // Refresh the displayed list of students after a successful addition
+async function addStudent(event) {
+  try {
+    event.preventDefault();
+    const studentName = document.getElementById('studentName').value;
+    const courseFee = document.getElementById('courseFee').value;
+    const courseTitle = document.getElementById('courseTitle').value;
+    const batchNumber = document.getElementById('batchNumber').value;
 
-  // Show a success notification using Notyf
+    const newStudent = {
+      name: studentName,
+      courseFee: courseFee,
+      courseTitle: courseTitle,
+      batchNumber: batchNumber,
+    };
+
+    // Make a POST request to add the new student to the server (Replace '/api/students' with your API endpoint)
+    const response = await axios.post('/api/students', newStudent);
+    const addedStudent = response.data;
+
+    studentsData.push(addedStudent);
+    renderStudents();
+
+    // Show a success notification using Notyf
+    notyf.success('Student added successfully!');
+  } catch (error) {
+    console.error('Error adding student:', error);
+    notyf.error('Failed to add student.');
+  }
 }
 
 // Function to edit a student
-function editStudent(studentId) {
-  // Implement code to handle the edit functionality for a specific student record
-  // Update the student data on the server (e.g., API) using axios
-  // Refresh the displayed list of students after a successful edit
+async function editStudent(studentId) {
+  try {
+    const studentToUpdate = studentsData.find((student) => student.id === studentId);
+    if (!studentToUpdate) return;
 
-  // Show a success notification using Notyf
+    // Implement the logic to get the updated student data from the user (e.g., using a form or prompt)
+    const updatedStudentData = {
+      // Implement the fields to update
+      name: prompt('Enter updated student name:', studentToUpdate.name),
+      courseFee: prompt('Enter updated course fee:', studentToUpdate.courseFee),
+      courseTitle: prompt('Enter updated course title:', studentToUpdate.courseTitle),
+      batchNumber: prompt('Enter updated batch number:', studentToUpdate.batchNumber),
+    };
+
+    // Make a PUT request to update the student data on the server (Replace `/api/students/${studentId}` with your API endpoint)
+    await axios.put(`/api/students/${studentId}`, updatedStudentData);
+
+    // Update the local data
+    Object.assign(studentToUpdate, updatedStudentData);
+    renderStudents();
+
+    // Show a success notification using Notyf
+    notyf.success('Student updated successfully!');
+  } catch (error) {
+    console.error('Error updating student:', error);
+    notyf.error('Failed to update student.');
+  }
 }
 
 // Function to delete a student
-function deleteStudent(studentId) {
-  // Implement code to handle the delete functionality for a specific student record
-  // Remove the student data from the server (e.g., API) using axios
-  // Refresh the displayed list of students after a successful deletion
+async function deleteStudent(studentId) {
+  try {
+    // Make a DELETE request to remove the student from the server (Replace `/api/students/${studentId}` with your API endpoint)
+    await axios.delete(`/api/students/${studentId}`);
 
-  // Show a success notification using Notyf
+    // Remove the student from the local data
+    studentsData = studentsData.filter((student) => student.id !== studentId);
+    renderStudents();
+
+    // Show a success notification using Notyf
+    notyf.success('Student deleted successfully!');
+  } catch (error) {
+    console.error('Error deleting student:', error);
+    notyf.error('Failed to delete student.');
+  }
 }
 
 // Function to search for students
 function searchStudents(searchQuery) {
-  // Implement code to filter the studentsData array based on the searchQuery
-  // Display the filtered results in the dataContainer
+  const filteredStudents = studentsData.filter((student) => {
+    const lowerCaseName = student.name.toLowerCase();
+    return lowerCaseName.includes(searchQuery);
+  });
+  renderFilteredStudents(filteredStudents);
+}
+
+// Function to render filtered students
+function renderFilteredStudents(filteredStudents) {
+  dataContainer.innerHTML = ''; // Clear the previous data
+  filteredStudents.forEach((student) => {
+    const studentElement = document.createElement('div');
+    studentElement.innerHTML = `
+      <p>Student Name: ${student.name}</p>
+      <p>Course Fee: ${student.courseFee}</p>
+      <p>Course Title: ${student.courseTitle}</p>
+      <p>Batch Number: ${student.batchNumber}</p>
+      <hr>
+    `;
+    dataContainer.appendChild(studentElement);
+  });
 }
 
 // Initialize Notyf for notifications
@@ -56,9 +149,17 @@ const dataContainer = document.getElementById('data');
 const searchInput = document.getElementById('searchInput');
 
 // Event listener for form submission to add a new student
-addStudentForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  addStudent();
+addStudentForm.addEventListener('submit', addStudent);
+
+// Event delegation for handling edit and delete buttons
+dataContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('edit-button')) {
+    const studentId = event.target.getAttribute('data-id');
+    editStudent(studentId);
+  } else if (event.target.classList.contains('delete-button')) {
+    const studentId = event.target.getAttribute('data-id');
+    deleteStudent(studentId);
+  }
 });
 
 // Event listener for search input
