0120a596c48750d79450c215bb9b9ad28514cf48
Kamran Raza <aipctoner@gmail.com>
2023-07-24 10:51:38 +0500

    code update

--- server.mjs
+++ server.mjs
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 const nanoid = customAlphabet("1234567890", 20);
 
 const mongodbURI = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.CLUSTER_NAME}/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`;
-
+//const mongodbURI = "mongodb+srv://aipectowner:Sultan7872@cluster0.jsjpffh.mongodb.net/?retryWrites=true&w=majority"
 const client = new MongoClient(mongodbURI);
 
 client.connect()
