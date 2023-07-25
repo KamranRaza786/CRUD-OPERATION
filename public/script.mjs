9957ccfe56a1cb522f0d72621c91b39547dffe89
Kamran Raza <137030916+KamranRaza786@users.noreply.github.com>
2023-07-26 03:57:04 +0500

    edit

--- public/script.mjs
+++ public/script.mjs
@@ -1,60 +1,95 @@
-async function editProduct(productId) {
+const { customAlphabet } = require("nanoid");
+
+const generateProductId = customAlphabet("1234567890", 10);
+
+document.getElementById("productForm").addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById("product.name").value;
+  const brand = document.getElementById("product.brand").value;
+  const model = document.getElementById("product.model").value;
+  const price = document.getElementById("product.price").value;
+
+  const newProduct = {
+    name: name,
+    brand: brand,
+    model: model,
+    price: parseFloat(price), // Parse price as a float
+  };
+
   try {
-    const response = await fetch(`/products/${productId}`);
+    const response = await fetch("/product", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newProduct),
+    });
+
+    if (response.ok) {
+      document.getElementById("productForm").reset();
+      fetchProducts();
+    } else {
+      alert("Failed to add product. Please try again.");
+    }
+  } catch (error) {
+    console.error("Error adding product:", error);
+    alert("Failed to add product. Please try again.");
+  }
+});
+
+async function fetchProducts() {
+  try {
+    const response = await fetch("/products");
     const data = await response.json();
 
     if (response.ok) {
-      const product = data;
-      const nameInput = document.getElementById("product.name");
-      const brandInput = document.getElementById("product.brand");
-      const modelInput = document.getElementById("product.model");
-      const priceInput = document.getElementById("product.price");
+      const productList = data.data;
+      const productTable = document.getElementById("productList").getElementsByTagName("tbody")[0];
+      productTable.innerHTML = "";
 
-      // Prepopulate the form fields with the product data
-      nameInput.value = product.name;
-      brandInput.value = product.brand;
-      modelInput.value = product.model;
-      priceInput.value = product.price;
-
-      // Add an "Update" button to submit the edited data
-      const updateButton = document.createElement("button");
-      updateButton.textContent = "Update";
-      updateButton.addEventListener("click", async () => {
-        const updatedProduct = {
-          name: nameInput.value,
-          brand: brandInput.value,
-          model: modelInput.value,
-          price: parseFloat(priceInput.value),
-        };
-
-        try {
-          const updateResponse = await fetch(`/products/${productId}`, {
-            method: "PUT",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updatedProduct),
-          });
-
-          if (updateResponse.ok) {
-            fetchProducts();
-          } else {
-            alert("Failed to update product. Please try again.");
-          }
-        } catch (error) {
-          console.error("Error updating product:", error);
-          alert("Failed to update product. Please try again.");
-        }
+      productList.forEach((product) => {
+        const row = productTable.insertRow();
+        row.innerHTML = `
+          <td>${product.id}</td>
+          <td>${product.name}</td>
+          <td>${product.brand}</td>
+          <td>${product.model}</td>
+          <td>${product.price}</td>
+          <td>
+            <button onclick="editProduct('${product.id}')">Edit</button>
+            <button onclick="deleteProduct('${product.id}')">Delete</button>
+          </td>
+        `;
       });
-
-      // Replace the existing "Add Product" button with the "Update" button
-      const addProductButton = document.getElementById("add-product-button");
-      addProductButton.parentNode.replaceChild(updateButton, addProductButton);
     } else {
-      console.log("Error fetching product details:", data.message);
+      console.log("Error fetching products:", data.message);
     }
   } catch (error) {
-    console.error("Error fetching product details:", error);
-    alert("Failed to fetch product details. Please try again.");
+    console.error("Error fetching products:", error);
   }
 }
+
+async function deleteProduct(productId) {
+  try {
+    const response = await fetch(`/products/${productId}`, {
+      method: "DELETE",
+    });
+
+    if (response.ok) {
+      fetchProducts();
+    } else {
+      alert("Failed to delete product. Please try again.");
+    }
+  } catch (error) {
+    console.error("Error deleting product:", error);
+    alert("Failed to delete product. Please try again.");
+  }
+}
+
+async function editProduct(productId) {
+  // Implement your code here for updating a product
+}
+
+// Call fetchProducts when the page loads to populate the table with existing products
+document.addEventListener("DOMContentLoaded", fetchProducts);
